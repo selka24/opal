c79e13a8d5caf711ec8373926279c9c7cb8a06ed
selka24 <92512624+selka24@users.noreply.github.com>
2023-10-12 17:11:19 +0200

    work with us component

--- components/home/WorkWithUs.vue
+++ components/home/WorkWithUs.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mt-36 relative">
-        <NuxtIcon name="circle" class="hidden sm:block text-[376px] absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 z-1" filled/>
-        <div class="flex flex-col items-center  z-2 relative">
+        <NuxtIcon name="circle" class="text-[376px] absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2" filled/>
+        <div class="flex flex-col text-center items-center relative z-10">
             <h1>Have a project in mind?</h1>
             <h1>Let's get to work.</h1>
             <NuxtLink to="/contact" class="mt-16">
